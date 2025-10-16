1dca390e5098bf68442c7f46ba5af8aab0e97bb4
Iarlley <iarlleymangueira@gmail.com>
2025-10-16 13:10:43 -0300

    Adicionado esqueleto do projeto

---
+++ src/main/java/com/EngCode/BFF_Agendador_de_Tarefas/BffAgendadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.EngCode.BFF_Agendador_de_Tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EngCode/BFF_Agendador_de_Tarefas/BffAgendadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EngCode.BFF_Agendador_de_Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
